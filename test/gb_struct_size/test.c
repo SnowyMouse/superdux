f86d2b3820ffd7cc5d4089f51b53146797f1c88d
Snowy <snowy@snowymouse.com>
2021-09-30 13:50:28 -0500

    Remove check for C vs C++ struct size since that issue was fixed as well

--- test/gb_struct_size/test.c
+++
@@ -1,6 +0,0 @@
-#include <Core/gb.h>
-#include <stdint.h>
-
-size_t get_gb_struct_size_c() {
-    return sizeof(struct GB_gameboy_s);
-}
--- test/gb_struct_size/test.cpp
+++
@@ -1,9 +0,0 @@
-extern "C" {
-#include <Core/gb.h>
-}
-
-#include <cstdint>
-
-extern "C" std::size_t get_gb_struct_size_cxx() {
-    return sizeof(GB_gameboy_s);
-}
