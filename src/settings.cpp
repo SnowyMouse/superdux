99705e66c7799676ccc3f3e1983a3afcab1e9979
Snowy <snowy@snowymouse.com>
2021-12-12 13:15:32 -0600

    Windows builds should be portable

--- src/settings.cpp
+++ src/settings.cpp
@@ -1,5 +1,10 @@
+#include <QApplication>
 #include "settings.hpp"
 
 QSettings get_superdux_settings() {
+#ifdef _WIN32
+    return QSettings(QCoreApplication::applicationDirPath().append("\\SuperDUX.ini"), QSettings::Format::IniFormat);
+#else
     return QSettings(QSettings::Format::IniFormat, QSettings::Scope::UserScope, "SnowyMouse", "SuperDUX");
+#endif
 }
