caad37b43994ca8ef6983e31a5d3d0ec693223d0
Snowy <snowy@snowymouse.com>
2021-12-11 21:57:22 -0600

    Have resolutions be stored in descending order for the icon

--- icon/generate.sh
+++ icon/generate.sh
@@ -6,5 +6,5 @@ convert kamonegi.png        -resize 64x64   icon_64.png
 convert kamonegi.png        -resize 32x32   icon_32.png
 convert kamonegi.png        -resize 16x16   icon_16.png
 
-convert icon_16.png icon_32.png icon_64.png icon_128.png icon_256.png kamonegi_shadow.png icon.ico
+convert icon_256.png icon_128.png icon_64.png icon_32.png icon_16.png icon.ico
 rm icon_*.png
