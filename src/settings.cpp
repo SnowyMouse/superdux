1bf286bb9d42245622e980aedb766f794279dd63
Snowy <snowy@snowymouse.com>
2021-12-19 21:23:08 -0600

    Set organization to SuperDUX so the config goes in the right folder on Linux

--- src/settings.cpp
+++ src/settings.cpp
@@ -5,6 +5,6 @@ QSettings get_superdux_settings() {
 #ifdef _WIN32
     return QSettings(QCoreApplication::applicationDirPath().append("\\SuperDUX.ini"), QSettings::Format::IniFormat);
 #else
-    return QSettings(QSettings::Format::IniFormat, QSettings::Scope::UserScope, "SnowyMouse", "SuperDUX");
+    return QSettings(QSettings::Format::IniFormat, QSettings::Scope::UserScope, "SuperDUX", "SuperDUX");
 #endif
 }
